67b1885fbb8340b58a9a65cae06437d1da1ea006
Emma Kanewoff <emmakanewoff@gmail.com>
2025-09-23 09:44:33 +0200

    Enhance BookingButton and HomePage with modal functionality and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import BookingButton from "../components/BookingButton";
 import BookingForm from "../components/BookingForm";
 
 export default function HomePage () {
+  const [isOpen, setIsOpen] = useState(false);
     
   return (
     <div className="container">
         <h1>Välkommen till Core Gym Club</h1>
         <p>Här kan du boka pass, se schema och hantera din profil</p>
 
-        {/* Lägger den här tillfälligt tills man kan lägga den på korten */}
-        <BookingButton buttonText="Boka nu" />
 
-        <BookingForm /> 
+        {/* Lägger den här tillfälligt tills man kan lägga den på korten */}
+        <BookingButton buttonText="Boka nu" onClick={() => setIsOpen(true)} />
+
+        {isOpen && (
+          <div className="modal-overlay">
+            <div className="modal-content">
+              <button className="close-btn" onClick={() => setIsOpen(false)}>X</button>
+              <BookingForm />
+            </div>
+          </div>
+        )}
+        
+
+        {/* <BookingForm />  */}
     </div>
   )
 }
