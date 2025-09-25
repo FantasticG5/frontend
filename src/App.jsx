050f4f528a35701a72e552bf1b1dafffdc624932
Emma Kanewoff <emmakanewoff@gmail.com>
2025-09-25 11:43:14 +0200

    Add Navbar component and styles for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import HomePage from "./pages/HomePage";
 import MyBookings from "./pages/MyBookings";
 import "./styles.css";
 import Sessions from "./pages/Sessions";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/bookings" element={<MyBookings />} />
